3f21782ecf2fdd94ac37d752347428efd67a0bbe
fallen-icarus <modern.daidalos+git@gmail.com>
2023-03-08 13:25:08 -0500

    Added another test

--- test/Test/CloseAsk.hs
+++ test/Test/CloseAsk.hs
@@ -289,13 +289,80 @@ closeWithoutBurning = do
           ]
       }
 
+closeMultipleAsks :: EmulatorTrace ()
+closeMultipleAsks = do
+  h1 <- activateContractWallet (knownWallet 1) endpoints
+
+  let borrowerPubKey = mockWalletPaymentPubKeyHash $ knownWallet 1
+      askDatum = AskDatum'
+        { askBeacon' = (beaconPolicySymbol,"Ask")
+        , borrowerId' = (beaconPolicySymbol,pubKeyAsToken borrowerPubKey)
+        , loanAsset' = (adaSymbol,adaToken)
+        , loanPrinciple' = 100_000_000
+        , loanTerm' = 12000
+        , collateral' = [testToken1]
+        }
+      addr = Address (ScriptCredential loanValidatorHash)
+                     (Just $ StakingHash
+                           $ PubKeyCredential
+                           $ unPaymentPubKeyHash
+                           $ mockWalletPaymentPubKeyHash
+                           $ knownWallet 1)
+  
+  callEndpoint @"ask" h1 $
+    AskParams
+      { askBeaconsMinted = [("Ask",1)]
+      , askBeaconRedeemer = MintAskToken' borrowerPubKey
+      , askBeaconPolicy = beaconPolicy
+      , askAddress = addr
+      , askInfo = 
+          [ ( Just askDatum
+            , lovelaceValueOf 2_000_000 <> singleton beaconPolicySymbol "Ask" 1)
+          ]
+      , askAsInline = True
+      }
+
+  void $ waitUntilSlot 2
+
+  callEndpoint @"ask" h1 $
+    AskParams
+      { askBeaconsMinted = [("Ask",1)]
+      , askBeaconRedeemer = MintAskToken' borrowerPubKey
+      , askBeaconPolicy = beaconPolicy
+      , askAddress = addr
+      , askInfo = 
+          [ ( Just askDatum
+            , lovelaceValueOf 2_100_000 <> singleton beaconPolicySymbol "Ask" 1)
+          ]
+      , askAsInline = True
+      }
+
+  void $ waitUntilSlot 4
+
+  callEndpoint @"close-ask" h1 $
+    CloseAskParams
+      { closeAskBeaconsBurned = [("Ask",-2)]
+      , closeAskBeaconRedeemer = BurnBeaconToken'
+      , closeAskBeaconPolicy = beaconPolicy
+      , closeAskLoanVal = loanValidator
+      , closeAskLoanAddress = addr
+      , closeAskSpecificUTxOs = 
+          [ ( askDatum
+            , lovelaceValueOf 2_000_000 <> singleton beaconPolicySymbol "Ask" 1
+            )
+          , ( askDatum
+            , lovelaceValueOf 2_100_000 <> singleton beaconPolicySymbol "Ask" 1
+            )
+          ]
+      }
+
 -------------------------------------------------
 -- Test Function
 -------------------------------------------------
 tests :: TestTree
 tests = do
   let opts = defaultCheckOptions & emulatorConfig .~ emConfig
-  testGroup "Close an ask"
+  testGroup "Close ask(s)"
     [ checkPredicateOptions opts "Fail if staking credential did not approve"
         (Test.not assertNoFailedTransactions) stakingCredDidNotApprove
     , checkPredicateOptions opts "Fail if not all ask beacons burned"
@@ -304,9 +371,11 @@ tests = do
         (Test.not assertNoFailedTransactions) wrongDatumType
     , checkPredicateOptions opts "Fail if ask beacon from address not burned"
         (Test.not assertNoFailedTransactions) closeWithoutBurning
-    , checkPredicateOptions opts "Successfully close ask"
+    , checkPredicateOptions opts "Successfully close single ask"
         assertNoFailedTransactions closeSingleAsk
+    , checkPredicateOptions opts "Successfully close multiple asks"
+        assertNoFailedTransactions closeMultipleAsks
     ]
 
 testTrace :: IO ()
-testTrace = runEmulatorTraceIO' def emConfig closeWithoutBurning
+testTrace = runEmulatorTraceIO' def emConfig closeMultipleAsks
