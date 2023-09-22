a9c8472ce5c63c8e2ac50d0d9b8741fe035a5b51
fallen-icarus <modern.daidalos+github@gmail.com>
2023-09-22 09:40:06 -0400

    docs: added docs to haddock generated docs for test-suite

--- test/Test.hs
+++ test/Test.hs
@@ -17,6 +17,19 @@ import Test.UpdateLenderAddress as UpdateLenderAddress
 import Test.Unlock as Unlock
 import Test.Proxy as Proxy
 
+{- | Tests are broken into 5 categories:
+
+(1) Regression Tests - tests that prove a certain feature actually works.
+
+(2) Failure Tests - tests that prove a certain action will fail.
+
+(3) Edge Case Tests - tests of unusual usages that should or should not fail.
+
+(4) Benchmark Tests - tests that will fail if the performance of the dApp decreases.
+
+(5) Performance Increase Tests - tests that will fail if the performance of the dApp increases.
+
+-}
 main :: IO ()
 main = do
   -- print $ scriptSize $ loanScript
